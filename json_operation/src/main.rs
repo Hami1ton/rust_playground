8845b3e10913ed703d00df7b555ac7664480820e
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-06-24 19:37:26 +0900

    デシリアライズの検証

---
+++ json_operation/src/main.rs
@@ -0,0 +1,13 @@
+extern crate serde_json;
+use std::fs::File;
+
+
+fn main() {
+    let data_json = File::open("asset/sample.json").unwrap();
+    println!("{:?}", &data_json);
+    
+    // serde_json のValue型にデシリアライズ
+    let data: serde_json::Value = serde_json::from_reader(&data_json).unwrap();
+
+    println!("data = {:?}", data);
+}
