b2f56ab471bb9ff197fe393acf52d44fd6100324
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2024-08-31 16:25:36 +0900

    add migration files

---
+++ actix_web_db/sqlx_sample/migrations/20240831071916_user.up.sql
@@ -0,0 +1,8 @@
+-- Add up migration script here
+CREATE TABLE user (
+    id integer primary key,
+    name text
+);
+
+insert into user (id, name) values (1, 'Taro');
+insert into user (id, name) values (2, 'Jiro');
