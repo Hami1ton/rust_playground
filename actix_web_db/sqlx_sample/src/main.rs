1eed921e508f9b74467b2de4f52b93282a87ed0d
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2024-09-14 16:16:36 +0900

    user一覧の取得

--- actix_web_db/sqlx_sample/src/main.rs
+++ actix_web_db/sqlx_sample/src/main.rs
@@ -2,6 +2,19 @@ use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 use actix_web::middleware::Logger;
 use env_logger::Env;
+use sqlx::sqlite::SqlitePool;
+use std::env;
+use dotenv::dotenv;
+use sqlx::Error;
+use sqlx::Sqlite;
+use sqlx::Pool;
+
+
+#[derive(sqlx::FromRow, Debug)]
+pub struct User {
+    pub id: i64,
+    pub name: String,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -10,8 +23,16 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
 
     env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    let pool = init_db();
+    let users = sqlx::query_as::<_, User>("SELECT * FROM user").fetch_all(&pool.await.unwrap()).await;
+
+    if let Ok(user) = users {
+        println!("{:?}", user);
+    }
 
     HttpServer::new(|| {
         App::new()
@@ -22,3 +43,11 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
+
+async fn init_db() -> Result<Pool<Sqlite>, Error> {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = SqlitePool::connect(&database_url).await.unwrap();
+    
+    return Ok(pool);
+}
+
