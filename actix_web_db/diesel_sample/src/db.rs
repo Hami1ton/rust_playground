417421fc730e58a91426c3a383bf077fe2ef3b9b
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2024-09-22 18:43:45 +0900

    apiでユーザ一覧を取得できる

--- actix_web_db/diesel_sample/src/db.rs
+++ actix_web_db/diesel_sample/src/db.rs
@@ -1,13 +1,14 @@
 use diesel::{prelude::*};
-use std::env;
 
 use crate::model::User;
 
-pub async fn get_users(conn: &mut SqliteConnection) {  //-> Vec<User>
+pub async fn get_users(conn: &mut SqliteConnection) -> Vec<User> { 
     use crate::schema::user::dsl::user;
 
     let get_users = user
         .select(User::as_select())
         .load(conn);
-    println!("{:?}", get_users);
+    // println!("{:?}", get_users);
+    
+    return get_users.expect("error");
 }
--- actix_web_db/diesel_sample/src/main.rs
+++ actix_web_db/diesel_sample/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, get, App, HttpResponse, HttpServer, Responder};
 use actix_web::middleware::Logger;
 use env_logger::Env;
 use dotenv::dotenv;
@@ -10,43 +10,43 @@ mod model;
 mod schema;
 
 
+type DbPool = r2d2::Pool<r2d2::ConnectionManager<SqliteConnection>>;
+pub struct AppState {
+    db_pool: DbPool,
+}
+
 #[get("/")]
-async fn hello() -> impl Responder {
+async fn get_users(data: web::Data<AppState>) -> impl Responder {
+    let mut conn = data.db_pool.get().unwrap();
+    let users = db::get_users(&mut conn).await;
+
+    HttpResponse::Ok().json(users)
+}
+
+#[get("/add-user")]
+async fn add_user(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok().body("OK")
 }
 
-
 #[actix_web::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
     // init db
     let pool = initialize_db_pool();
-    let mut conn = pool.get().unwrap();
-    db::get_users(&mut conn).await;
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState { db_pool: pool.clone() }))
+            .service(get_users)
+            .service(add_user)
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
-
-
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     dotenv().ok();
-//     // init db
-//     let pool = initialize_db_pool();
-
-//     env_logger::init_from_env(Env::default().default_filter_or("info"));
-
-//     HttpServer::new(|| {
-//         App::new()
-//             .service(hello)
-//             .wrap(Logger::default())
-//     })
-//     .bind(("127.0.0.1", 8080))?
-//     .run()
-//     .await
-// }
-
-
-
-type DbPool = r2d2::Pool<r2d2::ConnectionManager<SqliteConnection>>;
 
 fn initialize_db_pool() -> DbPool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
